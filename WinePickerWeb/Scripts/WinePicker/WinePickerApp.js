e479620791465f4367b57a2a5420c1d031cee17e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-03-11 23:28:24 +0000

    Modified jtCarouselCycle directive to take an expression

--- WinePickerWeb/Scripts/WinePicker/WinePickerApp.js
+++ WinePickerWeb/Scripts/WinePicker/WinePickerApp.js
@@ -4,50 +4,46 @@
 /// <reference path="WineDetailsController.js" />
 
 angular.module("WinePickerApp", [/* requires */])
-    .config(["$routeProvider", function ($routeProvider) {
+    .config(["$routeProvider", function($routeProvider) {
         console.log(".config() function");
         $routeProvider
             .when("/search", {})
             .when("/searchResults", { templateUrl: "AngularJSTemplates/WinePicker/SearchResults.html", controller: SearchResultsController })
             .when("/wines/:id", { templateUrl: "AngularJSTemplates/WinePicker/WineDetails.html", controller: WineDetailsController })
             .otherwise({ redirectTo: "/search" });
-    } ])
-    .factory("urlBuilder", function () {
+    }])
+    .factory("urlBuilder", function() {
         console.log(".factory() function");
         var apiKey = "2fd879a5765785c043cc992b550d2bda";
         var urlBuilder = new wineApi.UrlBuilder(apiKey);
         return urlBuilder;
     })
-    .directive("jtExternalLink", function () {
+    .directive("jtExternalLink", function() {
         console.log(".directive() function for jtExternalLink");
         var directiveDefinitionObject = {
             restrict: "A",
             replace: true,
             template: "<span>&nbsp;(at wine.com&nbsp;<img src='/Content/Images/Icon_External_Link.png' title='External link' alt='External link' />)</span>",
-            link: function (scope, iElement, iAttrs) {
+            link: function(scope, element, attrs) {
                 console.log("jtExternalLink postLink()");
                 console.dir(arguments);
             }
         };
         return directiveDefinitionObject;
     })
-    .directive("jtCarouselCycle", function () {
+    .directive("jtCarouselCycle", function() {
         console.log(".directive() function for jtCarouselCycle");
         var directiveDefinitionObject = {
             restrict: "A",
-            link: function (scope, iElement, iAttrs) {
-                console.log("jtCarouselCycle postLink()");
-                console.dir(arguments);
-                scope.$watch("products", function () {
-                    console.log("jtCarouselCycle $watch function for 'products'");
-                    var $element = $(iElement);
-                    if (scope.products !== null && scope.products.List.length > 0) {
-                        console.log("calling $element.carousel('cycle')");
-                        $element.carousel("cycle");
-                    } else {
-                        console.log("calling $element.carousel('pause')");
-                        $element.carousel("pause");
-                    }
+            link: function(scope, element, attrs) {
+                var expr = attrs.jtCarouselCycle;
+                console.log("jtCarouselCycle postLink() - expr: '" + expr + "'");
+                scope.$watch(expr, function(value) {
+                    console.log("jtCarouselCycle $watch function for '" + expr + "' - value: " + value);
+                    var carouselAction = (value) ? "cycle" : "pause";
+                    console.log("calling $element.carousel('" + carouselAction + "')");
+                    var $element = $(element);
+                    $element.carousel(carouselAction);
                 });
             }
         };
