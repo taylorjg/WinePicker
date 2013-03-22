b00990917b3a61a41d9425f935ae3bdc708b7dbb
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-03-22 13:35:15 +0000

    Added Bootstrap pagination to the search results - but it doesn't do anything yet

--- WinePickerWeb/Scripts/WinePicker/Models.js
+++ WinePickerWeb/Scripts/WinePicker/Models.js
@@ -2,4 +2,5 @@
     this.currentSlideNumber = 0;
     this.offset = 0;
     this.size = 10;
+    this.pages = [];
 };
--- WinePickerWeb/Scripts/WinePicker/SearchResultsController.js
+++ WinePickerWeb/Scripts/WinePicker/SearchResultsController.js
@@ -9,6 +9,15 @@ function SearchResultsController($scope, $location, searchResultsModel) {
     console.log("SearchResultsController - $location.path(): " + $location.path());
 
     $scope.model = searchResultsModel;
+
+    var page = 1;    
+    for (var i = 0; i < $scope.products.Total; i = i + $scope.model.size) {
+        $scope.model.pages.push(page);
+        page = page + 1;
+        if (page > 10) {
+            break;
+        }
+    }
 }
 
 SearchResultsController.$inject = ["$scope", "$location", "searchResultsModel"];
