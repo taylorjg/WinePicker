cd44f557854e37d2146dd9103d7473c574f68a98
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-03-28 01:16:15 +0000

    Fixed some of the failing unit tests after moving code around - a few more still to fix

--- WinePickerWeb/Scripts/WinePicker/JasmineTests/SearchCriteriaControllerSpec.js
+++ WinePickerWeb/Scripts/WinePicker/JasmineTests/SearchCriteriaControllerSpec.js
@@ -36,7 +36,63 @@ describe("SearchCriteriaController", function () {
     afterEach(function () {
     });
 
-    it("placeholder test", function () {
-        expect(1).toBe(1);
+    var numRefinements = function (categoryId) {
+        var filteredCategories = _.filter(wineApi.menuData.Categories, function (c) { return c.Id === categoryId; });
+        return filteredCategories[0].Refinements.length;
+    };
+
+    it("initial model values are correct", function () {
+
+        expect(_.isArray(_scope.wineTypes)).toBe(true);
+        expect(_scope.wineTypes.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_WINETYPE));
+
+        expect(_.isArray(_scope.varietals)).toBe(true);
+        expect(_scope.varietals.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_VARIETAL));
+
+        expect(_.isArray(_scope.regions)).toBe(true);
+        expect(_scope.regions.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_REGION));
+
+        expect(_.isArray(_scope.appellations)).toBe(true);
+        expect(_scope.appellations.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_APPELLATION));
+
+        expect(_scope.searchCriteriaModel.wineType).toBe("");
+        expect(_scope.searchCriteriaModel.varietal).toBe("");
+        expect(_scope.searchCriteriaModel.region).toBe("");
+        expect(_scope.searchCriteriaModel.appellation).toBe("");
+        expect(_scope.searchCriteriaModel.searchTerm).toBe("");
+        expect(_scope.searchCriteriaModel.state).toBe("");
+        expect(_scope.searchCriteriaModel.instock).toBe("");
+        expect(_scope.searchCriteriaModel.priceFrom).toBe("");
+        expect(_scope.searchCriteriaModel.priceTo).toBe("");
+        expect(_scope.searchCriteriaModel.ratingFrom).toBe("");
+        expect(_scope.searchCriteriaModel.ratingTo).toBe("");
+        expect(_scope.searchCriteriaModel.sortOrder).toBe("popularity");
+        expect(_scope.searchCriteriaModel.sortDirection).toBe("descending");
+        //expect(_scope.products).toBeNull();
+        
+        expect(_scope.moreSearchCriteriaCollapsed).toBe(true);
+    });
+
+    describe("getLargeLabelImageUrlForProduct method", function () {
+
+        it("given a product with an image URL ending with m.jpg returns a URL ending with l.jpg", function () {
+            var product = {
+                Labels: [{
+                    Url: "http://cache.wine.com/12345m.jpg"
+                }]
+            };
+            var url = _scope.getLargeLabelImageUrlForProduct(product);
+            expect(url).toBe("http://cache.wine.com/12345l.jpg");
+        });
+
+        it("given a product with an image URL not ending with m.jpg returns the image URL unchanged", function () {
+            var product = {
+                Labels: [{
+                    Url: "http://cache.wine.com/12345.jpg"
+                }]
+            };
+            var url = _scope.getLargeLabelImageUrlForProduct(product);
+            expect(url).toBe(product.Labels[0].Url);
+        });
     });
 });
--- WinePickerWeb/Scripts/WinePicker/JasmineTests/WinePickerControllerSpec.js
+++ WinePickerWeb/Scripts/WinePicker/JasmineTests/WinePickerControllerSpec.js
@@ -20,46 +20,10 @@ describe("WinePickerController", function () {
         _controller = $controller(WinePickerController, { $scope: _scope });
     }));
 
-    afterEach(function () {
-    });
-
-    var numRefinements = function (categoryId) {
-        var filteredCategories = _.filter(wineApi.menuData.Categories, function (c) { return c.Id === categoryId; });
-        return filteredCategories[0].Refinements.length;
-    };
-
     it("initial model values are correct", function () {
-
-        expect(_.isArray(_scope.wineTypes)).toBe(true);
-        expect(_scope.wineTypes.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_WINETYPE));
-
-        expect(_.isArray(_scope.varietals)).toBe(true);
-        expect(_scope.varietals.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_VARIETAL));
-
-        expect(_.isArray(_scope.regions)).toBe(true);
-        expect(_scope.regions.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_REGION));
-
-        expect(_.isArray(_scope.appellations)).toBe(true);
-        expect(_scope.appellations.length).toBe(numRefinements(wineApi.constants.CATEGORY_ID_APPELLATION));
-
         expect(_scope.wineApiCallInProgress).toBe(false);
         expect(_scope.errorMessagesVisible).toBe(false);
         expect(_scope.errorMessages).toBeNull();
-        expect(_scope.selectedWineType).toBe("");
-        expect(_scope.selectedVarietal).toBe("");
-        expect(_scope.selectedRegion).toBe("");
-        expect(_scope.selectedAppellation).toBe("");
-        expect(_scope.searchTerm).toBe("");
-        expect(_scope.moreSearchCriteriaCollapsed).toBe(true);
-        expect(_scope.state).toBe("");
-        expect(_scope.instock).toBe(false);
-        expect(_scope.priceFrom).toBe("");
-        expect(_scope.priceTo).toBe("");
-        expect(_scope.ratingFrom).toBe("");
-        expect(_scope.ratingTo).toBe("");
-        expect(_scope.sortOrder).toBe("popularity");
-        expect(_scope.sortDirection).toBe("descending");
-        expect(_scope.products).toBeNull();
     });
 
     describe("showErrorMessages", function () {
@@ -123,29 +87,6 @@ describe("WinePickerController", function () {
                 expect(_scope.errorMessages).toBeNull();
                 expect(_scope.errorMessagesVisible).toBe(false);
             });
-        });
-    });
-
-    describe("getLargeLabelImageUrlForProduct method", function () {
-
-        it("given a product with an image URL ending with m.jpg returns a URL ending with l.jpg", function () {
-            var product = {
-                Labels: [{
-                    Url: "http://cache.wine.com/12345m.jpg"
-                }]
-            };
-            var url = _scope.getLargeLabelImageUrlForProduct(product);
-            expect(url).toBe("http://cache.wine.com/12345l.jpg");
-        });
-
-        it("given a product with an image URL not ending with m.jpg returns the image URL unchanged", function () {
-            var product = {
-                Labels: [{
-                    Url: "http://cache.wine.com/12345.jpg"
-                }]
-            };
-            var url = _scope.getLargeLabelImageUrlForProduct(product);
-            expect(url).toBe(product.Labels[0].Url);
         });
     });
 });
