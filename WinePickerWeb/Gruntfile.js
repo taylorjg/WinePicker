94566325f31c688739b988a1478fced608c8c50b
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-04-09 11:12:17 +0100

    Moved jshint options into a separate .jshintrc file

--- WinePickerWeb/Gruntfile.js
+++ WinePickerWeb/Gruntfile.js
@@ -3,60 +3,10 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['Scripts/WinePicker/**/*.js'],
-            // See http://www.jshint.com/docs/
-            options: {
-                undef: true,
-                unused: true,
-                bitwise: true,
-                curly: true,
-                eqnull: true,
-                eqeqeq: true,
-                quotmark: "double",
-                trailing: true,
-                maxdepth: 4,
-                strict: true,
-                globals: {
-
-                    // My stuff
-                    wineApi: true,
-                    WineApiProxy: true,
-                    WinePickerController: true,
-                    SearchCriteriaController: true,
-                    SearchResultsController: true,
-                    WineDetailsController: true,
-                    WinePickerModel: true,
-                    SearchCriteriaModel: true,
-                    SearchResultsModel: true,
-                    WineDetailsModel: true,
-                    CriteriaFormatter: true,
-                    CriteriaParser: true,
-
-                    // underscore
-                    _: true,
-                    
-                    // jQuery
-                    $: true,
-                    
-                    // Web browser bits
-                    console: true,
-                    window: true,
-                    
-                    // Jasmine
-                    describe: true,
-                    it: true,
-                    beforeEach: true,
-                    afterEach: true,
-                    
-                    // AngularJS
-                    angular: true,
-                    browser: true,
-                    input: true,
-                    element: true,
-                    select: true,
-                    expect: true
-                }
-            }
+			options: grunt.file.readJSON('.jshintrc'),
+            files: [
+				'Scripts/WinePicker/**/*.js'
+			]
         },
         watch: {
             files: ['<%= jshint.files %>'],
