c454691d665ee9be5d462d01f0fac02b784a69de
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-04-13 12:58:30 +0100

    Added constants for the separators in the criteria strings

--- WinePickerWeb/Scripts/WinePicker/Utils.js
+++ WinePickerWeb/Scripts/WinePicker/Utils.js
@@ -2,6 +2,11 @@
 
     "use strict";
 
+// ReSharper disable InconsistentNaming
+    var COMPONENT_SEPARATOR = "|";
+    var NAME_VALUE_SEPARATOR = ":";
+// ReSharper restore InconsistentNaming
+
     window.CriteriaFormatter = function (prefix) {
 
         this._criteria = "";
@@ -10,9 +15,9 @@
         this.addComponent = function (name, value) {
             if (name !== "" && value !== "") {
                 if (this._criteria !== "") {
-                    this._criteria = this._criteria + "|";
+                    this._criteria = this._criteria + COMPONENT_SEPARATOR;
                 }
-                this._criteria = this._criteria + name + ":" + value;
+                this._criteria = this._criteria + name + NAME_VALUE_SEPARATOR + value;
             }
         };
 
@@ -25,13 +30,13 @@
     window.CriteriaParser = function (criteria) {
 
         this._criteria = criteria || "";
-        this._bits = this._criteria.split("|");
+        this._bits = this._criteria.split(COMPONENT_SEPARATOR);
 
         this.getComponent = function (name) {
             var value = "";
             for (var i = 0; i < this._bits.length; i++) {
                 var bit = this._bits[i];
-                var prefix = name + ":";
+                var prefix = name + NAME_VALUE_SEPARATOR;
                 if (bit.substring(0, prefix.length) === prefix) {
                     value = bit.substring(prefix.length);
                 }
